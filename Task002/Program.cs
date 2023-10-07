75acc89c4258579e6475f348a957c556a55299bd
AngieLuts <angiegk@yandex.ru>
2023-10-07 23:41:23 +0300

    first version for the task 2

---
+++ Task002/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Type in first number:");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Type in second number:");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Type in third number:");
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+int max = firstNumber;
+if (secondNumber > max)
+{
+    max = secondNumber;
+}
+else
+{
+    max = firstNumber;
+}
+if (thirdNumber > max)
+{
+    max = thirdNumber;
+}
+System.Console.WriteLine("max = " + max);
